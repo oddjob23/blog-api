9176b3f5d7628403e1183c1c63097e5f90b21af7
Milos Karaklajic <mkaraklajic@HBLP200123-01.local>
2020-05-12 12:37:36 +0200

    deployment setup

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%e2ceiqyx9y0e^%l9@gr#*_zzw)gkn1xd%@7danbvaa5&$*h5h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-54-165-234-81.compute-1.amazonaws.com']
 
 
 # Application definition
@@ -40,12 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     # 3rd party
     'rest_framework',
-    'rest_framework.authtoken',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'dj_rest_auth',
-    'dj_rest_auth.registration',
 
     # local
     'frontend',
@@ -93,8 +87,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'project',
+        'USER': 'zante23',
+        'PASSWORD': 'zante23',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -137,3 +135,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
